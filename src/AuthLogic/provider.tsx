a5b0c04a18c8500d36bcb1fb319335533d5de275
SethPattee <patteeseth@gmail.com>
2024-11-06 15:18:33 -0700

    attempt for redirect

--- src/AuthLogic/provider.tsx
+++ src/AuthLogic/provider.tsx
@@ -3,9 +3,11 @@ import { AuthProvider, AuthProviderProps, useAuth } from "react-oidc-context";
 
 const oidcConfig: AuthProviderProps = {
   authority: "https://auth.snowse.duckdns.org/realms/advanced-frontend/",
-  client_id: "seth-authdemo",
-  redirect_uri: "http://localhost:3000/",
-  silent_redirect_uri: "http://localhost:3000/silent-renew.html",
+  client_id: "seth-final",
+    // redirect_uri: "http://localhost:3000/", //change for kubernest 
+    redirect_uri: "https://sethstar.duckdns.org/",
+    silent_redirect_uri: "https://sethstar.duckdns.org/silent-renew.html",
+  // silent_redirect_uri: "http://localhost:3000/silent-renew.html",
   automaticSilentRenew: true,
   onSigninCallback: async (user) => {
     const newURL = window.location.href.split("?")[0];
@@ -21,7 +23,7 @@ function SessionChecker() {
   useEffect(() => {
     const checkRenewal = () => {
       if (auth.isAuthenticated && auth.user) {
-        const expiration = auth.user.expires_at;
+        const expiration = auth.user.expires_at || 0;
         const timeRemaining = expiration - Math.floor(Date.now() / 1000);
 
         if (timeRemaining < 300 && !renewalChecked) {
