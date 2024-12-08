074162fc64b82f615ca7848e9f89aa14ea563a59
SethPattee <patteeseth@gmail.com>
2024-12-07 18:31:27 -0700

    CORS

--- client/src/component/websockets/WebSocket.tsx
+++ client/src/component/websockets/WebSocket.tsx
@@ -4,7 +4,7 @@ function Websocket() {
   const [socket, setSocket] = useState<WebSocket | undefined>(undefined);
   const [inputMessage, setInputMessage] = useState<string>('');
   const [messages, setMessages] = useState<string[]>([]);
-  const websocketUrls = ['wss://localhost:5009/ws', 'wss://sethapi.duckdns.org/wss'];
+  const websocketUrls = ['ws://localhost:5009/ws', 'ws://sethapi.duckdns.org/ws'];
 
   useEffect(() => {
     let socketInstance: WebSocket | undefined;
